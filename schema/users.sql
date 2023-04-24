4b9b1e587fe4017db7434fb782858f812c5ee3dd
abdelrahman1427 <abdelrahman.abdalla@stemredsea.moe.edu.eg>
2023-04-24 23:42:49 +0200

    add docker and update database

--- schema/users.sql
+++ schema/users.sql
@@ -4,6 +4,8 @@ CREATE TABLE users (
   lastName VARCHAR(255) NOT NULL,
   age INT
 );
-INSERT INTO `users` VALUES ('abdelrahman','mo',35,'f88afa0a-1bcb-47fa-95a2-3c1a8056700a'),
-                        ('Ahmed','Mohamed',20,'1d877c7a-9c09-4d98-8cca-304088492a7f'),
-                        ('Ahmed','Ashraf',25,'f5422c13-1a4c-4754-9802-509f6040c380');
+
+INSERT INTO `users` VALUES 
+('f88afa0a-1bcb-47fa-95a2-3c1a8056700a', 'abdelrahman', 'mohamed', 35),
+('1d877c7a-9c09-4d98-8cca-304088492a7f', 'Ahmed', 'Mohamed', 20),
+('f5422c13-1a4c-4754-9802-509f6040c380', 'Ahmed', 'Ashraf', 25);
